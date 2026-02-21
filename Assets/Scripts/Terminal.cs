2197c2a29f352dcb86ec94d7cd2e11c52c455bdf
Alexis <90532166+computing-lex@users.noreply.github.com>
2026-02-21 13:21:56 -0500

    magic ui

--- Assets/Scripts/Terminal.cs
+++ Assets/Scripts/Terminal.cs
@@ -6,29 +6,64 @@ using UnityEngine.InputSystem;
 public class Terminal : MonoBehaviour
 {
     InputAction interact;
-    [SerializeField] private float interactDistance = 0.5f;
-    [SerializeField] private float viewDistance = 1;
+    [SerializeField] private float interactDistance = 2;
+    [SerializeField] private float viewDistance = 5;
+    [SerializeField] private float maxViewDistance = 110;
+    [SerializeField] private float minFontSize = 12;
+    [SerializeField] private float maxFontSize = 200;
     [SerializeField] private TMP_Text prompt;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float playerDistance = 100;
+
     void Start()
     {
-        interact = InputSystem.actions.FindAction("Interact");    
+        interact = InputSystem.actions.FindAction("Interact");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (interact.IsPressed())
+        playerDistance = Vector3.Distance(transform.position, PlayerController.Instance.transform.position);
+
+        if (interact.IsPressed() && playerDistance < interactDistance)
         {
-            GameManager.Instance.OnDelivery();   
+            GameManager.Instance.OnDelivery();
         }
 
-        float playerDistance = Vector3.Distance(transform.position, PlayerController.Instance.transform.position);
-        
+        if (playerDistance > maxViewDistance)
+        {
+            prompt.text = "!";
+        }
+        else if (playerDistance > interactDistance)
+        {
+            prompt.text = playerDistance.ToString("F1") + " m";
+        }
+        else
+        {
+            prompt.text = "Deposit Data\n[E]";
+        }
+
+        /*  FONT SCALING
+        At 100 m away, the player is at the max distance, so the font should
+        be at its max size. When at 10 or closer, the font is at minimum.
+        To simplify, call it 110 m. So the font needs to vary from min to max
+        over 100 units. Distance is alrerady stored in playerDistance 
+        */
+
         if (playerDistance < viewDistance)
         {
-            prompt.text = playerDistance + " m";
+            prompt.fontSize = minFontSize;
+        }
+        else if (playerDistance < maxViewDistance)
+        {
+            /* if view < playerDistance < max
+               0 < playerDistance - view < max - view
+               0 < (playerDistance - view) / (max - view) < 1
+               wow math */
+            prompt.fontSize = Mathf.Lerp(minFontSize, maxFontSize, (playerDistance - viewDistance)/(maxViewDistance-viewDistance));
+        }
+        else
+        {
+            prompt.fontSize = maxFontSize;
         }
     }
 
